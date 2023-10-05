b56ca59b3620974ce2aadd0af4b145e80241cc7c
AaronKibysz <142132061+AaronKibysz@users.noreply.github.com>
2023-10-04 23:21:53 -0300

    Ejercicios Complementarios Clase 1

---
+++ ejercicios_Complementarios1.py
@@ -0,0 +1,57 @@
+# Ejercicio 1
+numero1 = 7 
+# Ejercicio 2
+numero2 = 3.1416
+# Ejercicio 3
+suma = numero1 + numero2
+print(suma)
+# Ejercicio 4
+resta = numero1 - numero2
+print(resta)
+
+multiplicacion = numero1 * numero2
+print(multiplicacion)
+
+if( numero2 != 0 ):
+    division = numero1 / numero2
+    print(division)
+# Ejercicio 5
+nombre = 'Aaron Kibysz'
+# Ejercicio 6
+precio = 123.56
+# Ejercicio 7
+descuento = 0.78
+# Ejercicio 8
+precio_final = precio-(precio*descuento)
+print(precio_final)
+# Ejercicio 9
+cadena = 'Hola Mundo'
+# Ejercicio 10
+longitud = len(cadena)
+print(longitud)
+# Ejercicio 11
+precio = 987.65
+precio = int(precio)
+# Ejercicio 12
+nombre = 'Aaron'
+apellido = 'Kibysz'
+nombre_completo = nombre + ' ' + apellido
+print(nombre_completo)
+# Ejercicio 13
+edad = 21
+edad += 5
+edad -= 10
+print(edad)
+# Ejercicio 14
+altura = 1.80
+altura *= 4
+altura /= 3
+print(altura)
+# Ejercicio 15
+nombre = 'AARON'
+nombre = nombre.lower()
+print(nombre)
+# Ejercicio 16
+nombre = nombre.upper()
+nombre = nombre.title()
+print(nombre)
