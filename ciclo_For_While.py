21064ab39873ed7a75d3a3682412f88ac3afa691
AaronKibysz <142132061+AaronKibysz@users.noreply.github.com>
2023-10-05 10:42:44 -0300

    Trabajo en Clase 2

---
+++ ciclo_For_While.py
@@ -0,0 +1,48 @@
+#EJERCICIO 1
+
+abc_tuple = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+
+corrimiento = int(input('Ingrese el numero del corrimiento: '))
+
+message_list = []
+for i in range(5):
+    message_list.append(input('Ingrese mensaje '))
+    message_list[i] = message_list[i].upper()
+
+for i in range(len(message_list)):
+    message_aux = message_list[i]
+    message_aux2 = ''
+    
+    for letter in message_aux:
+        if letter not in abc_tuple:
+            message_aux2 += letter
+        else:
+            index = abc_tuple.index(letter)
+            new_index = (index + corrimiento) % len(abc_tuple)
+            message_aux2 +=  abc_tuple[new_index]
+    print(message_aux2)
+
+#EJERCICIO 2
+
+number_list = []
+print('Ingrese numeros enteros positivos o 0 para finalizar')
+x = -1
+while x < 0 or x>0:
+    number_element = int(input())
+    if number_element == 0:
+        break
+    elif number_element < 0:
+        continue
+    else:
+        number_list.append(number_element)
+
+even = 0
+not_even = 0;
+for element in range(len(number_list)):
+    if number_list[element] %2 == 0:
+        even += 1
+    else:
+        not_even += 1
+
+print('Se han ingresado : ', even, 'Numeros Pares')
+print('Se han ingresado : ', not_even, 'Numeros Impares')
