d558ebb5f107d171df1fcaf096b7c76dd832db6e
AaronKibysz <aaronkibysz@gmail.com>
2023-10-19 11:49:57 -0300

    todo commiteado

--- ejercicio_suma_cifras/Test_ejercicio_suma_digitos.py
+++ ejercicio_suma_cifras/Test_ejercicio_suma_digitos.py
@@ -1,11 +1,11 @@
 import pytest
 from suma_cifras import number_addition
 
-@pytest.mark.paranetrize('a, res',[
+@pytest.mark.parametrize("number, res",[
     (12,3),
     (99,18),
     (10,1),
-    45,9
+    (45,9),
 ])
-def test_cifras(a, res):
-    assert number_addition(a) == res
+def test_cifras(number, res):
+    assert number_addition(number) == res
