e2f2643b724ab8e752954418e83a7878749173fd
AaronKibysz <aaronkibysz@gmail.com>
2023-10-26 08:51:23 -0300

    Commit final

--- TP5/ejercicio16/digit_search.py
+++ TP5/ejercicio16/digit_search.py
@@ -1,6 +1,2 @@
 def digit_search_in_number(number, digit):
-    coincidence_count = 0
-    for i in str(number):
-        if i == str(digit):
-            coincidence_count += 1
-    return coincidence_count
+    return str(number).count(str(digit))
